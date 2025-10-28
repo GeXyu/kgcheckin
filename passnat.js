cc1cb64b22df9317c3bdf4ed00d69c471b253cdd
ge <861719405@qq.com>
2025-10-28 15:31:56 +0800

    .

--- passnat.js
+++ passnat.js
@@ -3,19 +3,21 @@ import { post, get } from "./utils/passnat_api.js";
 async function passnat() {
     const phone = process.env.PASSNAT_PHONE
     const password = process.env.PASSNAT_PASSWORD
-    console.log("phone:%s password:%s", phone, password)
 
     // 先登录
     const res = await post("/public/login", { "password": password, "phone_number": phone, "platform": 1 })
-    console.log(res)
+
     
     // // 如果登录成功，执行签到
-    // if (res.code === 0) {
-    //     const token = res.data.token
-    //     // 设置token到环境变量或请求头中
-    //     const checkin = await post("/user/checkIn", { "token": token })
-    //     console.log(checkin)
-    // }
+    if (res.code === 10000) {
+        console.log("登录成功")
+        const auth_token = res.data.auth_token
+        // 设置token到环境变量或请求头中
+        const checkin = await post("/user/checkIn", { "authorization": 'Bearer ' + auth_token})
+        console.log(checkin)
+    }else { 
+        console.log("登录失败")
+    }
 }
 
 passnat()
