e8d3373d074d77b50da22a9e9a8ce8f54768746f
ge <861719405@qq.com>
2025-10-28 15:28:29 +0800

    .

--- passnat.js
+++ passnat.js
@@ -1,17 +1,21 @@
-import { post } from "./utils/passnat_api.js";
+import { post, get } from "./utils/passnat_api.js";
 
 async function passnat() {
-
     const phone = process.env.PASSNAT_PHONE
     const password = process.env.PASSNAT_PASSWORD
-    console.log("phone:%s password:%s",phone,password)
+    console.log("phone:%s password:%s", phone, password)
 
-    //先登录
-    const res = await post("/public/login",  {"password":password,"phone_number":phone,"platform":1})
+    // 先登录
+    const res = await post("/public/login", { "password": password, "phone_number": phone, "platform": 1 })
     console.log(res)
-
-    // const checkin = await get("/user/checkIn",  {})
-    // console.log(checkin)
+    
+    // // 如果登录成功，执行签到
+    // if (res.code === 0) {
+    //     const token = res.data.token
+    //     // 设置token到环境变量或请求头中
+    //     const checkin = await post("/user/checkIn", { "token": token })
+    //     console.log(checkin)
+    // }
 }
 
 passnat()
--- utils/passnat_api.js
+++ utils/passnat_api.js
@@ -1,6 +1,6 @@
 
 // 发送请求
-async function get(path,headers) {
+async function get(path, headers) {
   const result = await fetch("https://api.passnat.com" + path, {
     method: "GET",
     headers: headers
@@ -10,12 +10,15 @@ async function get(path,headers) {
 }
 
 
-async function post(path,body) {
+async function post(path, body) {
   const result = await fetch("https://api.passnat.com" + path, {
     method: "POST",
-    body: body
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(body)
   }).then(r => r.json())
-  console.log("post body:%s",body)
+  console.log("post body:%s", JSON.stringify(body))
   return result
 }
 
