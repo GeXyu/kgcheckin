5273b021f1bfd6af476d7b33961ec26078af0b0e
ge <861719405@qq.com>
2025-11-15 14:15:53 +0800

    .

--- enshan.js
+++ enshan.js
@@ -12,7 +12,7 @@ async function passnat() {
 
 
 
-    headers = {
+    const headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:85.0) Gecko/20100101 Firefox/85.0',
         'Connection' : 'keep-alive',
         'Host' : 'www.right.com.cn',
