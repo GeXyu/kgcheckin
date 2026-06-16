35b30692d28d7a9047da9daecfef800ba5454e09
ge <861719405@qq.com>
2026-06-16 17:17:40 +0800

    1

---
+++ tianfu.js
@@ -0,0 +1,35 @@
+import { daily_claim } from "./utils/passnat_api.js";
+import { delay } from "./utils/utils.js";
+import DingTalkNotifier from "./utils/dingtalk.js";
+async function tianfu() {
+    const auth_token = process.env.TIANFU_TOKEN
+    // 获取钉钉webhook地址和密钥（从环境变量中获取）
+    const dingtalkWebhook = process.env.DINGTALK_WEBHOOK
+    const dingtalkSecret = process.env.DINGTALK_SECRET
+
+    // 创建钉钉通知实例（仅在配置了webhook时创建）
+    const dingtalkNotifier = dingtalkWebhook ? new DingTalkNotifier(dingtalkWebhook, dingtalkSecret) : null;
+
+    // 先登录
+    const res = await daily_claim(auth_token)
+
+    
+    // // 如果登录成功，执行签到
+    if (res.code === 200) {
+        console.log("签到成功")
+    }else { 
+        console.log("登录失败")
+        if (dingtalkNotifier) {
+            try {
+            await dingtalkNotifier.sendWithTitle(
+                "注意!",
+                "tianfu登陆失败!"
+            );
+            } catch (notifyError) {
+            console.error("钉钉通知发送失败:", notifyError);
+            }
+        }
+    }
+}
+
+tianfu()
---
+++ utils/tianfu_api.js
@@ -0,0 +1,15 @@
+
+
+async function daily_claim(token) {
+  const result = await fetch("https://api.tianfuagent.com/user/api/v1/user/user/credits/daily-claim", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+      "authorization": token
+    },
+
+  }).then(r => r.json())
+  return result
+}
+
+export { daily_claim }
